4806cf4aa6bdc90224c4417d0f031f852ba188b5
Ferdysan <ferdysan87@gmail.com>
2019-05-20 15:53:21 +0200

    genero le prime 3 cifre del codice del giocatore

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
-// Milestone 1
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde
+// Il software deve generare casualmente le statistiche di gioco di
+// 100 giocatori di basket per una giornata di campionato.
+// In particolare vanno generate per ogni giocatore le seguenti
+// informazioni, facendo attenzione che il numero generato abbia
+// senso:
+// - Codice Giocatore Univoco (formato da 3 lettere maiuscole casuali e 3 numeri)
+// - Numero di punti fatti
+// - Numero di rimbalzi
+// - Falli
+// - Percentuale di successo per tiri da 2 punti
+// - Percentuale di successo per tiri da 3 punti
+
+
+//definisco le variabili
+
+var caratteri ='ABCDEFGHILMNOPQRSTUVZ';
+
+// elaboro la funzione per la creazione del Giocatore
+function genera_giocatore (){
+  //mi definisco una variabile in cui inserire i caratteri generati random concatenati con i numeri
+  var codice_giocatore='';
+
+  for (var i = 0; i < 3; i++) {
+    //genero una variabile che avrà ad ogni ciclo un numero diverso
+    var posizione_carattere = generatoreRandom(0, 20);
+    var carattere = caratteri.charAt(posizione_carattere);
+  
+    codice_giocatore += carattere;
+  }
+  return codice_giocatore
+}
+
+var test = genera_giocatore ();
+
+console.log(test);
+
+//Funzione genera random
+function generatoreRandom(min, max) {
+  return Math.floor(Math.random()*(max - min + 1))+ min;
+}
