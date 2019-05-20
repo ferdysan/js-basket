f5f4725fb02b43cbf4e10311128d97e11d988382
Ferdysan <ferdysan87@gmail.com>
2019-05-20 16:14:06 +0200

    genero 100 oggetti giocatore

--- main.js
+++ main.js
@@ -14,6 +14,9 @@
 //definisco le variabili
 
 var caratteri ='ABCDEFGHILMNOPQRSTUVZ';
+//definisco un array in cui andrò ad inserire i 100 giocatori generati casualmente dalla mia funzione genera giocatore
+var giocatori_basket =[];
+
 
 // elaboro la funzione per la creazione del Giocatore
 function genera_giocatore (){
@@ -23,16 +26,44 @@ function genera_giocatore (){
   for (var i = 0; i < 3; i++) {
     //genero una variabile che avrà ad ogni ciclo un numero diverso
     var posizione_carattere = generatoreRandom(0, 20);
+    //uso quella variabile come indice per il metodo charAt che mi andrà a pescare un carattere dalla variabile caratteri
     var carattere = caratteri.charAt(posizione_carattere);
-  
+    //concateno ogni carattere trovato alla variabile codice giocatore
     codice_giocatore += carattere;
   }
-  return codice_giocatore
+  //genero 3 cifre e le concateno alla variabile codice giocatore
+  for(var i =0; i<3; i++){
+    //alla variabile giocatore ora concateno 3 numeri
+   codice_giocatore += generatoreRandom(0, 9);
+  }
+
+  //genero 3 variabili per le statistiche giocatore
+  var punti_fatti = generatoreRandom(0,100);
+  var rimbalzi = generatoreRandom(50, 200);
+  var falli = generatoreRandom(0,100);
+
+  //genero le percentuali
+  var perc2= (generatoreRandom(0,1000)/10).toFixed(1);
+  var perc3= (100 - perc2).toFixed(1);
+
+  //con tutti i dati a disposizione creo un oggetto giocatore
+  var giocatore={
+    'codice' : codice_giocatore,
+    'punti' : punti_fatti,
+    'rimbalzi': rimbalzi,
+    'falli' : falli,
+    'tiri2' : perc2,
+    'tiri3' : perc3
+  }
+  return giocatore
 }
 
-var test = genera_giocatore ();
+for(var i=0; i< 100; i++){
+  var giocatore = genera_giocatore();
+  giocatori_basket.push(giocatore);
+}
+console.log(giocatori_basket)
 
-console.log(test);
 
 //Funzione genera random
 function generatoreRandom(min, max) {
